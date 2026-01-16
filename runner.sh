6918214e2dc2e1fe3a5bf1467a15bea65b2f5d37
cadurda <caleb@durda.org>
2026-01-16 13:30:42 -0600

    Add shell script

---
+++ runner.sh
@@ -0,0 +1,11 @@
+SOURCE_FILE = "hello_world.cpp"
+EXECUTABLE = "hello_world"
+
+g++ "$SOURCE_FILE" -o "$EXECUTABLE"
+
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Running the program:"
+    ./"$EXECUTABLE"
+else
+    echo "Compilation failed."
+fi
