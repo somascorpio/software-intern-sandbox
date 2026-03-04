e28c56601bd6f25065bc9b1e0e468772ccee2add
Tim V <tim.vasenkov@gmail.com>
2026-03-04 14:55:50 -0600

    creates more unit tests for power_led

--- tests/test_power_led.cpp
+++ tests/test_power_led.cpp
@@ -8,3 +8,22 @@ TEST(PowerLedTest, PackSetsPowerCorrectly)
 
     EXPECT_EQ(buf[0], 1);  // power on
 }
+
+TEST(PowerLedTest, PackSetsVoltageCorrectly)
+{
+    PowerLed led{2.5, 1};
+    Buf buf = pack(led);
+
+    EXPECT_EQ(buf[1], '2'); //2
+    EXPECT_EQ(buf[2],'5'); //.5
+    EXPECT_EQ(buf[3],'0');//.00
+}
+
+TEST(PowerLedTest, UnpackWorksCorrectly){
+    PowerLed led{2.5, 1};
+    Buf buf = pack(led);
+    PowerLed led1 = unpack(buf);
+
+    EXPECT_EQ(led1.power,1);
+    EXPECT_EQ(led1.voltage,2.5);
+}
