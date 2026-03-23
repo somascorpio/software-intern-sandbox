07018160ff06dc89bf6db7dccec293aa29bf36c5
Intern <tim.vasenkov@gmail.com>
2026-03-23 16:06:45 -0500

    creates ability to store integer array of size 3, representing color

--- examples/power_led.cpp
+++ examples/power_led.cpp
@@ -5,11 +5,15 @@
 #include "power_led.hpp"
 
 int main(){
-    PowerLed message = {1.6,1}; //"on", 1.6v-3.5v
+    PowerLed message = {1.6,1,{255,125,0}}; //"on", 1.6v-3.5v
     Buf buf = pack(message); //storing an integer in a vector of 4 bytes of data (serializing)
     PowerLed deserialized = unpack(buf); //deserializing the byte vector back into an integer stored in a PowerLed struct
     printBuff(buf); //prints the 4 bytes of data out
     std::cout << "\nDeserialized power:" << deserialized.power << std::endl; //prints the deserialized integer value
-    std::cout << "\nDeserialized voltage: " << deserialized.voltage << std::endl;
+    std::cout << "\nDeserialized voltage: " << deserialized.voltage << std::endl << "\nDeserialized color: " << std::endl;
+    
+    for(int i = 0;i < 3; i++){
+        std::cout << "\t" << std::dec << (int)deserialized.rgb[i] << std::endl;
+    }
     return 0;
 }
